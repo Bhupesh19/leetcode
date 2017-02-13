ca2b87ca9766b2a10223b60ee95f8d6172343fe0
jiujieti <melody2014ymq@gmail.com>
2017-02-13 21:54:00 +0100

    10.......

--- 10-backtrack.java
+++ 10-backtrack.java
@@ -1,46 +1,54 @@
 public class Solution {
-  private Map<Integer, Character> map = new HashMap<>();
+  private Set<Integer> indexSet = new HashSet<>();
   
   public boolean isMatch(String s, String p) {
-    if(p == null || s == null || p.length() == 0 || p.charAt(0) == '*') {
+    if(p == null || s == null) { 
+      return false;
+    }
+    if (p.length() == 0) {
+      return s.length() == 0;
+    }
+    if (p.charAt(0) == '*') {
       return false;
     }
     for(int i = 1; i < p.length(); i++) {
       if(p.charAt(i) == '*') {
         if(p.charAt(i-1) == '*') {
           return false;
-        }
-        map.put(i-1, p.charAt(i-1));
+        } 
+        indexSet.add(i-1);
       }
     }
     return isLayerMatch(s, p, 0, 0);
   }
 
-  public boolean isLayerMatch(String s, String p, int level, int index) {
+  private boolean isLayerMatch(String s, String p, int level, int index) {
     if(level == p.length()) {
       return index == s.length();
     }
     if(index == s.length()) {
-      if(level != p.length()) {
-        while(level < p.length()) {
-          if(!map.containsKey(level)) {
-            return false;
-          }
-          level += 2;  
+      while(level < p.length()) {
+        if(!indexSet.contains(level)) {
+          return false;
         }
+        level += 2;  
       }
       return true; 
     }
-    if(!map.containsKey(p.charAt(level))) {
-      if(s.charAt(index) != p.charAt(level) && p.charAt(level) != '.') {
+    if(!indexSet.contains(level)) {
+      if(!isCharMatch(s.charAt(index), p.charAt(level))) {
         return false;
       }
       return isLayerMatch(s, p, level+1, index+1);
     }
-    if(p.charAt(level) == s.charAt(index) || p.charAt(level) == '.') {
+    if(isCharMatch(s.charAt(index), p.charAt(level))) {
       return isLayerMatch(s, p, level, index+1) || isLayerMatch(s, p, level+2, index+1) || isLayerMatch(s, p, level+2, index);
     } else {
       return isLayerMatch(s, p, level+2, index);
     }
   }
+  
+  private boolean isCharMatch(char c1, char c2) {
+    return c1 == c2 || c2 == '.';
+  } 
 }
