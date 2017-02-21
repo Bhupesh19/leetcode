f31b7e81d62a21ee7df07c5943da552d10672e63
jiujieti <melody2014ymq@gmail.com>
2017-02-21 22:25:34 +0100

    Solution 24 added.

--- 24.java
+++ 24.java
@@ -1,4 +1,20 @@
 public class Solution {
   public ListNode swapPairs(ListNode head) {
+    if(head == null || head.next == null) {
+      return head;
+    }
+    ListNode p = new ListNode(0);
+    p.next = head;
+    head = head.next;
+    ListNode q = p.next;
+    while(p.next != null && p.next.next != null) {
+      ListNode temp = p.next;
+      p.next = p.next.next;
+      q = p.next.next;
+      p.next.next = temp;
+      temp.next = q;
+      p = temp;
+    }
+    return head; 
   } 
 }
