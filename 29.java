f3f2935fc324813edccf9a8c6cc3e30c5ad1403e
jiujieti <melody2014ymq@gmail.com>
2017-02-22 19:38:50 +0100

    Solution 29 in progress.

--- 29.java
+++ 29.java
@@ -1,4 +1,11 @@
 public class Solution {
   public int divide(int dividend, int divisor) {
+    if(divisor == 0) {
+      return Integer.MAX_VALUE;
+    }
+    int sign = 1;
+    if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+      sign = -1;
+    }
   }
 }
