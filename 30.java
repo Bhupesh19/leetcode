312a53aa1130ab30b778f7a83e40c0ec12d69975
jiujieti <melody2014ymq@gmail.com>
2017-02-23 21:10:47 +0100

    Solution 30 in progress.

--- 30.java
+++ 30.java
@@ -1,4 +1,20 @@
 public class Soluiton {
   public List<Integer> findSubstring(String s, String[] words) {
+    List<Integer> results = new ArrayList<>();
+    if(words.length == 0) {
+      return results;
+    }
+    if(words[0].length() == 0) {
+      for(int i = 0; i < s.length(); i++) {
+        results.add(i);
+      }
+      return results;
+    }
+     
+    return results;
   }
+
+  private void extend(String s, String[] words, int index, Map<String, > map, List<Integer> results) {
+  }
+  
 }
