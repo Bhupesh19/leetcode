2eb33ede29b2374491a330bb641081ca6d6904e6
jiujieti <melody2014ymq@gmail.com>
2017-02-23 23:55:01 +0100

    Add 35-binary.java.

--- 35-binary.java
+++ 35-binary.java
@@ -1,7 +1,21 @@
 public class Solution {
   public int searchInsert(int[] nums, int target) {
-    int index = -1;
-    for(int i = 0; i < nums.length; i++) {
+    if(nums.length == 0) {
+      return 0;
     }
+    int low = 0, high = nums.length - 1;
+    int mid = (low + high) / 2;
+    while(low < high) {
+      if(target <= nums[mid]) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+      mid = (low + high) / 2;
+    }
+    if(high == nums.length - 1 && target > nums[nums.length-1]) {
+      return nums.length;
+    }
+    return high;
   }
 }
