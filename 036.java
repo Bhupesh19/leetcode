d63d266170b1391c3b27c84be4f3e1f82ae727ae
jiujieti <melody2014ymq@gmail.com>
2017-02-27 19:39:51 +0100

    Solution 36 added.

--- 036.java
+++ 036.java
@@ -1,16 +1,21 @@
 public class Solution {
-  public boolean isValidSodoku(char[][] board) {
+  public boolean isValidSudoku(char[][] board) {
     for(int i = 0; i < 9; i++) {
       Set<Character> rowElems = new HashSet<>();
       Set<Character> colElems = new HashSet<>();
       Set<Character> cubElems = new HashSet<>();
       for(int j = 0; j < 9; j++) {
-        if(char[i][j] != '.' && !rowElems.add(char[i][j])) {
+        if(board[i][j] != '.' && !rowElems.add(board[i][j])) {
           return false; 
         }
-        if(char[j][i] != '.' && !colElems.add(char[j][i])) {
+        if(board[j][i] != '.' && !colElems.add(board[j][i])) {
           return false;
-        }  
+        }
+        int rowIndex = 3 * (i / 3);
+        int colIndex = 3 * (i % 3);
+        if(board[rowIndex+j/3][colIndex+j%3] != '.' && !cubElems.add(board[rowIndex+j/3][colIndex+j%3])) {
+          return false;
+        }
       }
     }
     return true; 
