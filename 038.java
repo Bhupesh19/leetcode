3345c56a2c7e59c1ac114afe7951d822439245f6
jiujieti <melody2014ymq@gmail.com>
2017-02-27 20:19:38 +0100

    Solution 38 added.

--- 038.java
+++ 038.java
@@ -1,10 +1,22 @@
 public class Solution {
   public String countAndSay(int n) {
-    String start = Integer.toString(n); 
     StringBuilder seq = new StringBuilder("");
-    seq.append(start);
+    String str = Integer.toString(1);
     for(int i = 1; i < n; i++) {
+      int counter = 1;
+      int j = 1;
+      seq.setLength(0);
+      for(j = 1; j < str.length(); j++) {
+        if(str.charAt(j) == str.charAt(j-1)) {
+          counter++;
+        } else {
+          seq.append(counter).append(str.charAt(j-1));
+          counter = 1;
+        }
+      }
+      seq.append(counter).append(str.charAt(j-1));
+      str = seq.toString();
     }
-    return seq.toString();
+    return str; 
   }
 }
