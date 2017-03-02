7ebfa86faa8f6a9e2f36364a81b39687ddce5048
jiujieti <melody2014ymq@gmail.com>
2017-03-02 21:35:32 +0100

    Solution 94 still under construction.

--- 094-iteration.java
+++ 094-iteration.java
@@ -4,7 +4,8 @@ public class Solution {
     List<Integer> results = new ArrayList<>();
     Stack<TreeNode> nodes = new Stack<>();
     TreeNode curr = root;
-    while(curr != null) {
+    while(true) {
+      if(curr == null )
       while(curr.left != null) {
         nodes.push(curr);
         curr = curr.left; 
@@ -14,7 +15,6 @@ public class Solution {
         curr = nodes.pop();
         results.add(curr.val);
         curr = curr.right;
-        results.add(curr.val);
       } else {
         break;
       }
