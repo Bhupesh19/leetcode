6bce9a4c5db74704eb30d7ff2ab71a68356a51ac
jiujieti <melody2014ymq@gmail.com>
2017-03-03 17:46:56 +0100

    Solution 56 added.

--- 056.java
+++ 056.java
@@ -1,19 +1,44 @@
 public class Solution {
   public List<Interval> merge(List<Interval> intervals) {
-    
+    sort(intervals, 0, intervals.size()-1); 
+    List<Interval> results = new ArrayList<>();
+    if(intervals.size() == 0) {
+      return results;
+    }
+    results.add(intervals.get(0));
+    for(int i = 1; i < intervals.size(); i++) {
+      Interval last = results.get(results.size()-1);
+      Interval curr = intervals.get(i);
+      if(curr.start > last.end) {
+        results.add(curr);
+      } else {
+        last.start = (last.start < curr.start) ? last.start : curr.start;
+        last.end = (last.end > curr.end) ? last.end : curr.end;
+      }
+    }
+    return results; 
   }
 
   private void sort(List<Interval> intervals, int low, int high) {
     if(low < high) {
-      q = partition(intervals, low, high);
+      int q = partition(intervals, low, high);
       sort(intervals, low, q-1);
       sort(intervals, q+1, high);
     }
     return; 
   }
 
-  private void partition(List<Interval> intervals, int low, int high) {
-    
+  private int partition(List<Interval> intervals, int low, int high) {
+    Interval pivot = intervals.get(high);
+    int j = low;
+    for(int i = low; i < high; i++) {
+      if(intervals.get(i).start <= pivot.start) {
+        swap(intervals, i, j);
+        j++;
+      }
+    }
+    swap(intervals, j, high);
+    return j; 
   }
 
   private void swap(List<Interval> intervals, int fst, int snd) {
