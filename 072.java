869990c1f5f441d2a67f6c06b0f0121cb35ba05b
jiujieti <melody2014ymq@gmail.com>
2017-03-08 19:19:53 +0100

    Solution 72 added.

--- 072.java
+++ 072.java
@@ -1,25 +1,25 @@
 public class Solution {
   public int minDistance(String word1, String word2) {
-    if(word1.length() == 0) {
-      return word2.length();
+    int[][] md = new int[word1.length()+1][word2.length()+1];
+    for(int i = 0; i <= word1.length(); i++) {
+      md[i][0] = i;  
     }
-    if(word2.length() == 0) {
-      return word1.length();
+    for(int j = 0; j <= word2.length(); j++) {
+      md[0][j] = j;  
     }
-    int[][] lcs = new int[word1.length()+1][word2.length()+1];
     for(int i = 1; i <= word1.length(); i++) {
       for(int j = 1; j <= word2.length(); j++) {
         if(word1.charAt(i-1) == word2.charAt(j-1)) {
-          lcs[i][j] = lcs[i-1][j-1] + 1;
-        } else if(lcs[i-1][j] >= lcs[i][j-1]) {
-          lcs[i][j] = lcs[i-1][j];
+          md[i][j] = md[i-1][j-1];
+        } else if(md[i-1][j] >= md[i][j-1] && md[i-1][j-1] >= md[i][j-1]) {
+          md[i][j] = md[i][j-1] + 1;
+        } else if(md[i][j-1] >= md[i-1][j] && md[i-1][j-1] >= md[i-1][j]){
+          md[i][j] = md[i-1][j] + 1;
         } else {
-          lcs[i][j] = lcs[i][j-1];
+          md[i][j] = md[i-1][j-1] + 1;
         }
       }
     }
-    int dis1 = Math.abs(lcs[word1.length()][word2.length()]-word1.length());
-    int dis2 = Math.abs(lcs[word1.length()][word2.length()]-word2.length());
-    return (dis1 > dis2) ? dis1 : dis2;
+    return md[word1.length()][word2.length()];
   }
 }
---
+++ 096.java
@@ -0,0 +1,8 @@
+public class Solution {
+  public int numTrees(int n) {
+    int[] numRootTrees = new int[n+1];
+    int[] numLeftTrees = new int[n+1]; 
+    int[] numRightTrees = new int[n+1];
+     
+  }
+}
