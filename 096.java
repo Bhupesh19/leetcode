280f927960afdc7a65ef12c4817b7b5ac1518a72
jiujieti <melody2014ymq@gmail.com>
2017-03-08 19:24:15 +0100

    Delete 96.

--- 096.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-  public int numTrees(int n) {
-    int[] numRootTrees = new int[n+1];
-    int[] numLeftTrees = new int[n+1]; 
-    int[] numRightTrees = new int[n+1];
-     
-  }
-}
