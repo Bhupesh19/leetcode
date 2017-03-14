c9cae245831fa31462332a7b380ee38c66870f1e
jiujieti <melody2014ymq@gmail.com>
2017-03-14 18:52:05 +0100

    Solution 79 in progress.

---
+++ 079.java
@@ -0,0 +1,14 @@
+public class Solution {
+  public boolean exist(char[][] board, String word) {
+    for(int i = 0; i < board.length; i++) {
+      for(int j = 0; j < board[0].length; j++) {
+        if(board[i][j] == word.charAt()) {
+        
+        }
+      }
+    }
+    return false;
+  }
+
+  private boolean searchWord() 
+}
