f72a583353e6e3e98cff282d2b875901a64b7217
jiujieti <melody2014ymq@gmail.com>
2017-03-22 20:59:41 +0100

    Solution 82 added.

--- 082.java
+++ 082.java
@@ -1,5 +1,22 @@
 public class Solution {
   public ListNode deleteDuplicates(ListNode head) {
-    
+    Map<Integer, Integer> map = new LinkedHashMap<>();
+    while(head != null) {
+      if(!map.containsKey(head.val)) {
+        map.put(head.val, 1);
+      } else { 
+        map.put(head.val, map.get(head.val) + 1);
+      }
+      head = head.next;
+    }
+    ListNode newHead = new ListNode(0);
+    ListNode q = newHead;
+    for(Integer cand : map.keySet()) {
+      if(map.get(cand) == 1) {
+        q.next = new ListNode(cand);
+        q = q.next;
+      }
+    }
+    return newHead.next;  
   }
 }
