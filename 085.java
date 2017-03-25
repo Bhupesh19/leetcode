d439909a0b816926380b8008cdd4558a57e569fb
jiujieti <melody2014ymq@gmail.com>
2017-03-25 12:39:36 +0100

    Solution 86 under construction.

---
+++ 085.java
@@ -0,0 +1,9 @@
+public class Solution {
+  public int maximalRectangle(char[][] matrix) {
+    if(matrix.length == 0) {
+      return 0;
+    }
+    int row = matrix.length;
+    int col = matrix[0].length;
+  }
+}
---
+++ 086.java
@@ -0,0 +1,26 @@
+public class Solution {
+  public ListNode partition(ListNode head, int x) {
+    ListNode newHead = new ListNode(0);
+    newHead.next = head;
+    ListNode p = newHead;
+    ListNode q = head;
+    while(q != null && q.val != x) {
+      if(p.next.val < x) {
+        p = p.next;
+      }
+      q = q.next;
+    }
+    while(q != null && q.next != null) {
+      if(q.next.val < x) {
+        ListNode temp1 = q.next.next;
+        ListNode temp2 = p.next;
+        p.next = q.next;
+        p.next.next = temp2;
+        q.next = temp1;   
+      } else {
+        q = q.next;
+      }
+    }
+    return newHead.next;
+  }
+}
