fb908fdc1310ad726995bbb4c4fa39c7bbfd4a36
jiujieti <melody2014ymq@gmail.com>
2017-03-28 23:36:46 +0200

    Solution 92 in progress.

--- 092.java
+++ 092.java
@@ -3,8 +3,18 @@ public class Solution {
     ListNode newHead = new ListNode(0);
     newHead.next = head; 
     ListNode p = newHead;
-    ListNode q = newHead;  
-    int i = 0;
+    ListNode q = head;  
+    for(int i = 0; i <= n; i++) {
+      if(i < m - 1) {
+        p = p.next;
+        q = q.next;
+      } else {
+        ListNode temp = q.next;
+        q.next = q.next.next;
+        temp.next = p.next;
+        p.next = temp;
+      }
+    } 
     return newHead.next; 
   }
 }
