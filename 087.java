939ad2fe9e825e1e3e5a611d56b19ec398ce7cac
jiujieti <melody2014ymq@gmail.com>
2017-03-28 20:27:46 +0200

    Solution 87 still no clues.

---
+++ 087.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+  private boolean match = false;
+
+  public boolean isScramble(String s1, String s2) {
+   
+  }
+
+  private boolean scramble(String s1, String s2) {
+  
+  }
+}
