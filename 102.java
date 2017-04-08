437b508e0750cac1d0bd08b21367980610903021
jiujieti <melody2014ymq@gmail.com>
2017-04-08 12:02:46 +0200

    Solution 102 added.

--- 102.java
+++ 102.java
@@ -1,5 +1,23 @@
 public class Solution {
+
+  private List<List<Integer>> res = new ArrayList<>();
+
   public List<List<Integer>> levelOrder(TreeNode root) {
-  
+    traverse(root, 0);
+    return res;
   }
+
+  private void traverse(TreeNode tree, int height) {
+    if(tree == null) {
+      return;
+    }
+    int h = height + 1;
+    if(res.size() < h) {
+      List<Integer> level = new ArrayList<>();
+      res.add(level);
+    }
+    res.get(height).add(tree.val);
+    traverse(tree.left, h);
+    traverse(tree.right, h); 
+  } 
 }
