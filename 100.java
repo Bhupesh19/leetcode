52098b6df7e7c6cf48b92486f8abff3067cda8bd
jiujieti <melody2014ymq@gmail.com>
2017-04-21 18:31:39 +0200

    Solution 100 improved.

--- 100.java
+++ 100.java
@@ -1,11 +1,8 @@
 public class Solution {
   public boolean isSameTree(TreeNode p, TreeNode q) {
-    if(p == null) {
-      return q == null;
+    if(p == null || q == null) {
+      return p == q;
     }
-    if(q == null || p.val != q.val) {
-      return false;
-    }
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
+    return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right); 
   }
 }
