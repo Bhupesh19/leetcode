224c8ae364813a0f572d9a2857ac3ad00b7b79f4
jiujieti <melody2014ymq@gmail.com>
2017-04-30 21:11:04 +0200

    Solution 110 added.

--- 110.java
+++ 110.java
@@ -1,4 +1,17 @@
 public class Solution {
   public boolean isBalanced(TreeNode root) {
+    if(root == null) {
+      return true;
+    }
+    return isBalanced(root.left) && isBalanced(root.right) && Math.abs(height(root.left)-height(root.right)) <= 1; 
+  }
+
+  private int height(TreeNode root) {
+    if(root == null) {
+      return 0;
+    }
+    int leftHeight = height(root.left) + 1;
+    int rightHeight = height(root.right) + 1;
+    return (leftHeight > rightHeight) ? leftHeight : rightHeight;
   }
 }
