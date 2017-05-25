978a2cf1ed5fe9c6c3f63cc4e91403872fa1c6af
jiujieti <melody2014ymq@gmail.com>
2017-05-25 13:52:24 +0200

    Solution 016 added

--- 016.java
+++ 016.java
@@ -1,1 +1,23 @@
-NO!!!!!!
+public class Solution {
+  public int threeSumClosest(int[] nums, int target) {
+    Arrays.sort(nums);
+    int res = nums[0] + nums[1] + nums[2];
+    for(int i = 0; i < nums.length - 2; i++) {
+      int low = i + 1, high = nums.length - 1;
+      while(low < high) {
+        int curr = nums[low] + nums[high] + nums[i];
+        if(curr == target) {
+          res = target;
+          return res;
+        }
+        res = (Math.abs(target - curr) < Math.abs(target - res)) ? curr : res; 
+        if(curr < target) {
+          low++;
+        } else {
+          high--;
+        }
+      }
+    }
+    return res;
+  }
+}
