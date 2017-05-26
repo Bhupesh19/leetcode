d9c0fd43307389edcc1fa4433b4c270047dda2d7
jiujieti <melody2014ymq@gmail.com>
2017-05-26 22:02:41 +0200

    Solution 203 added

---
+++ 203-recursion.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public ListNode removeElements(ListNode head, int val) {
+    if(head == null) {
+      return null;
+    }
+    if(head.val == val) {
+      return removeElements(head.next, val);
+    }
+    head.next = removeElements(head.next, val);
+    return head;
+  }
+}
