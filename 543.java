b47e3a3ca6d55e96006b95928dec19f539d3e510
jiujieti <melody2014ymq@gmail.com>
2017-07-19 21:57:44 +0200

    Solution 543 added

---
+++ 543.java
@@ -0,0 +1,18 @@
+public class Solution {
+  private int max = 0;
+
+  public int diameterOfBinaryTree(TreeNode root) {
+    depth(root);
+    return max;
+  }
+
+  private int depth(TreeNode root) {
+    if(root == null) {
+      return 0;
+    }
+    int l = depth(root.left);
+    int r = depth(root.right);
+    max = Math.max(max, l + r);
+    return Math.max(l, r) + 1;
+  }
+}
