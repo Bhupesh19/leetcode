c8460d7993c7dd3462a27132d85f23ec589e3a48
jiujieti <melody2014ymq@gmail.com>
2017-08-27 18:52:24 +0200

    Solution 662 added

---
+++ 572.java
@@ -0,0 +1,12 @@
+public class Solution {
+  public boolean isSubtree(TreeNode s, TreeNode t) {
+    if(s == null) return false;
+    return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+  }
+
+  private boolean isSame(TreeNode s, TreeNode t) {
+    if(t == null && s == null) return true;
+    if(t == null || s == null) return false;
+    return t.val == s.val && isSame(s.left, t) && isSame(s.right, t);
+  }
+}
