a85541eb2189fb42b3b4ea78ac7d3e5c97c9e618
jiujieti <melody2014ymq@gmail.com>
2017-08-30 23:50:15 +0200

    wip

--- WIP-636.java
+++ WIP-636.java
@@ -1,19 +1,15 @@
 class Solution {
   public int[] exclusiveTime(int n, List<String> logs) {
-    int interval = 0;
+    int prev = 0;
     Stack<String> stk = new Stack<>();
     int[] res = new int[n];
     for(String log: logs) {
-      if(log.indexOf("start") != -1) {
+      String[] time = log.split(":");
+      if(time[1] == "start") {
         stk.push(log);
       }
-      if(log.indexOf("end") != -1) {
-        String[] stime = (stk.pop()).split(":");
-        String[] etime = log.split(":");
-        int st = Integer.parseInt(stime[2]);
-        int et = Integer.parseInt(etime[2]); 
-        res[Integer.parseInt(stime[0])] += et - st + 1 - interval; 
-        interval = et - st + 1;
+      if(time[1] == "end") {
+
       }
     }
     return res;
