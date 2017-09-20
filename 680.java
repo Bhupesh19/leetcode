aad93f4a8db249e2a9d12eced7eead57c95cade1
jiujieti <melody2014ymq@gmail.com>
2017-09-20 12:18:36 +0200

    680 fixed

--- 680.java
+++ 680.java
@@ -1,20 +1,30 @@
 class Solution {
   public boolean validPalindrome(String s) {
-    boolean hasOne = false;
     int i = 0, j = s.length() - 1;
     while(i < j) {
       if(s.charAt(i) != s.charAt(j)) {
-        if(hasOne) return false;
-        hasOne = true;
-        if(j - 1 >= 0 && s.charAt(i) == s.charAt(j - 1)) {
-          --j;
-        } else if(i + 1 < s.length() && s.charAt(i + 1) == s.charAt(j)) {
-          ++i;
+        if(s.charAt(i + 1) == s.charAt(j) && isPalindrome(s, i + 1, j)) {
+          return true;
+        } else if(s.charAt(i) == s.charAt(j - 1) && isPalindrome(s, i, j - 1)) {
+          return true;
+        } else {
+          return false;
         }
       }
       ++i;
       --j;
     }
-  return true;
+    return true;
+  }
+
+  private boolean isPalindrome(String s, int i, int j) {
+    while(i < j) {
+      if(s.charAt(i) != s.charAt(j)) {
+        return false;
+      }
+      ++i;
+      --j;
+    }
+    return true;
   }
 }
