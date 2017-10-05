6d56c56d0983789b5b53534810e41b736902aa29
jiujieti <melody2014ymq@gmail.com>
2017-10-05 19:33:41 +0200

    Solution 687 added

--- 687.java
+++ 687.java
@@ -1,18 +1,20 @@
 class Solution {
+  
+  private int max = 0;
+
   public int longestUnivaluePath(TreeNode root) {
-    if(root == null) return 0;
-    int max = findPath(root.left, root.val) + findPath(root.right, root.val); 
-    int maxLeft = longestUnivaluePath(root.left);
-    int maxRight = longestUnivaluePath(root.right);
-    return Math.max(maxLeft, Math.max(maxRight, max)); 
+    if(root != null) {
+      dfs(root);
+    }
+    return max;
   }
 
-  public int findPath(TreeNode root, int last) {
-    if(root == null) return 0;
-    if(root.val == last) {
-      return Math.max(1 + findPath(root.left, root.val), 1 + findPath(root.right, root.val));
-    } else {
-      return 0;
-    }
+  private int dfs(TreeNode node) {
+    int l = (node.left != null) ? dfs(node.left) : 0;
+    int r = (node.right != null) ? dfs(node.right) : 0;
+    int resl = (node.left != null && node.left.val == node.val) ? l + 1 : 0;
+    int resr = (node.right != null && node.right.val == node.val) ? r + 1 : 0;
+    max = Math.max(max, resl + resr);
+    return Math.max(resl, resr);
   }
 }
