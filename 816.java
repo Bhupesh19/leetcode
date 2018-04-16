e0c8493d4e71bdca24aa0d2f34fb9ef09a356546
jiujieti <melody2014ymq@gmail.com>
2018-04-16 22:39:12 +0200

    816 added

---
+++ 816.java
@@ -0,0 +1,52 @@
+class Solution {
+  public List<String> ambiguousCoordinates(String S) {
+    // the basic idea to solve this problem is to first find where to put the comma first
+    // then use two arraylists, collect all possible left parts and right parts
+    List<String> results = new ArrayList<>();
+    for (int i = 1; i < S.length() - 2; i++) {
+      List<String> leftNums = new ArrayList<>();
+      List<String> rightNums = new ArrayList<>();
+      findValidNums(leftNums, S, 1, i);
+      findValidNums(rightNums, S, i + 1, S.length() - 2);
+      for (String a: leftNums) {
+        for (String b: rightNums) {
+          results.add("(" + a + ", " + b + ")");
+        }
+      }
+    }
+    return results;
+  }
+    
+  private void findValidNums(List<String> validNums, String S, int l, int r) {
+    if (l == r) {
+      validNums.add(S.substring(l, l + 1));
+      return;
+    }
+    String t = S.substring(l, r + 1);
+    if (hasNoLeadingZeroes(t)) {
+      validNums.add(t);
+    }
+    for (int i = l; i < r; i++) {
+      String temp = S.substring(l, i + 1) + "." + S.substring(i + 1, r + 1);
+      String integ = S.substring(l, i + 1);
+      String decimal = S.substring(i + 1, r + 1);
+      if (hasNoLeadingZeroes(integ) && hasNoTrailingZeroes(decimal)) {
+        validNums.add(temp);
+      }
+    }
+  }
+    
+  private boolean hasNoLeadingZeroes(String t) {
+    if (t.startsWith("0") && t.length() != 1) {
+      return false;
+    }
+    return true;
+  }
+    
+  private boolean hasNoTrailingZeroes(String t) {
+    if (t.endsWith("0")) {
+      return false;
+    }
+    return true;
+  }
+}
