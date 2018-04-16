aee2e8886a0f5dcf6d6909221e4cc82501f4ba02
jiujieti <melody2014ymq@gmail.com>
2018-04-16 20:59:24 +0200

    547 added

---
+++ 547.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int findCircleNum(int[][] M) {
+    boolean[] visited = new boolean[M.length];
+    int count = 0;
+    for (int i = 0; i < M.length; i++) {
+      if (!visited[i]) {
+        countCircles(M, visited, i);
+        count++;
+      }
+    }
+    return count;
+  }
+    
+  private void countCircles(int[][] M, boolean[] visited, int i) {
+    for (int j = 0; j < M.length; j++) {
+      if (M[i][j] == 1 && !visited[j]) {
+        visited[i] = true;
+        countCircles(M, visited, j);
+      }
+    }
+  }
+}
