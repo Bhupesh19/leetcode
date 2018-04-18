e5677ff9f70ce4194de97ae2532f28f67d3d0462
jiujieti <melody2014ymq@gmail.com>
2018-04-18 21:14:08 +0200

    468 lambda added

---
+++ 468-lambda.java
@@ -0,0 +1,59 @@
+class Solution {
+  public String validIPAddress(String IP) {
+    if (isIPv4(IP)) {
+      return "IPv4";
+    }
+    if (isIPv6(IP)) {
+      return "IPv6";
+    }
+    return "Neither";
+  }
+
+  private boolean isIPv4(String ip) {
+    String[] parts = ip.split("\\.", -1);
+    if (parts.length != 4) {
+      return false;
+    }
+    for (String part : parts) {
+      int number = 0;
+      for (int i = 0; i < part.length(); i++) {
+        char c = part.charAt(i);
+        if (c > '9' || c < '0') {
+          return false;
+        }
+        number = number * 10 + c - '0';
+      }
+      if (part.length() == 0 || part.length() > 3 || hasLeadingZeroes(part) || number > 255) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean hasLeadingZeroes(String s) {
+    return s.startsWith("0") && s.length() != 1;
+  }
+
+  private boolean isIPv6(String ip) {
+    String[] parts = ip.split(":", -1);
+    if (parts.length != 8) {
+      return false;
+    }
+    boolean b1 = false;
+    for (String part : parts) {
+      b1 = part.chars()
+              .mapToObj(c -> (char)c)
+              .map(c -> Character.toLowerCase(c))
+              .anyMatch(c -> (c > '9' || c < '0') && (c < 'a' || c > 'f'));
+      if (b1) {
+        return false;
+      }
+    }
+    boolean b2 = Arrays.stream(parts)
+            .anyMatch(s -> s.length() == 0 || s.length() > 4);
+    if (b2) {
+      return false;
+    }
+    return true;
+  }
+}
